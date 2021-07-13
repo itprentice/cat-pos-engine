a5a1f264751d2c54ff3acc066dede5ad3ba7ed81
Ian Prentice <29048123+itprentice@users.noreply.github.com>
2021-07-12 22:45:08 -0400

    Started the front end. Did the css for the main page.

---
+++ react_frontend/cat-pos-front/src/Launch.js
@@ -0,0 +1,10 @@
+const Launch = () => {
+    return (  
+        <div className="launch">
+            <h1>Cat Position Finder</h1>
+            <p>Please upload a picture of a cat resting to determine what position they are in.</p>
+        </div>
+    );
+}
+ 
+export default Launch;
---
+++ react_frontend/cat-pos-front/src/UploadPic.js
@@ -0,0 +1,10 @@
+const UploadPic = () => {
+    return (  
+        <form action="">
+            <input type="file"/>
+            {/* TODO: add a custom upload file button so that it takes up the same amount of space as the pciture will */}
+        </form>
+    );
+}
+ 
+export default UploadPic;
