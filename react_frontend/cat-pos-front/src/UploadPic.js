b99ff3f3e2bb974fd615191adb6b89094b172745
Ian Prentice <29048123+itprentice@users.noreply.github.com>
2021-10-07 20:11:31 -0400

    updated output message for predicting cat position

--- react_frontend/cat-pos-front/src/UploadPic.js
+++ react_frontend/cat-pos-front/src/UploadPic.js
@@ -19,7 +19,9 @@ const UploadPic = () => {
             body: data,
         }).then((response) => {
             response.text().then((text) => {
-                setPosition(text);
+                const textArray = text.split();
+                setPosition(textArray[0]);
+                setConfidence(textArray[1]);
             });
         });
     }
@@ -61,7 +63,7 @@ const UploadPic = () => {
             {/* <form action="">
                 {uploadFlag && <button>Find Cat Position</button>}
             </form> */}
-            <h2>{position}</h2>
+            <h2>This cat is in a {position} resting position. The model is {Math.round(confidence*10000)/10000 * 100} % confident about this.</h2>
         </div>
     );
 }
